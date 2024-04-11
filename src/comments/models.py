fcbb6d29deba30eea0134b7d7889660e7405a5d0
frutez <glebka.yatsenko@gmail.com>
2024-04-11 20:04:39 +0300

    create: comments

---
+++ src/comments/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+from ..news.models import News
+
+User = get_user_model()
+
+
+class Comment(models.Model):
+    text = models.TextField(max_length=1000)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='comments')
+    news = models.ForeignKey(News, on_delete=models.CASCADE, related_name='news_comments')
+
+    class Meta:
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        created_at_str = self.created_at.strftime('%Y-%m-%d %H:%M:%S')
+        return f'ID: {self.id}, Text: {self.text}, Author: {self.author}, Created at: {created_at_str}, News: {self.news}'
---
+++ src/comments/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Comment
+from ..user_app.serializers import UserSerializer
+
+class AuthorCommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'created_at', 'author', 'news')
+        extra_kwargs = {
+            'author': {'read_only': True},
+            'created_at': {'read_only': True}
+        }
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = UserSerializer()
+
+    class Meta:
+        model = Comment
+        fields = ('id', 'text', 'created_at', 'author', 'news')
