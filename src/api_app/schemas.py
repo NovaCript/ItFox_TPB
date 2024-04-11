a935b3914da806f6338fdf29f2ee6ca38f143ec9
frutez <glebka.yatsenko@gmail.com>
2024-04-11 16:17:04 +0300

    Custom:     User     BaseAuth - token     API_app

---
+++ src/api_app/schemas.py
@@ -0,0 +1,14 @@
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ItFox-News",
+        default_version='v1',
+        description="Тестовое задание для ItFox на Django Rest Framework",
+        contact=openapi.Contact(url="https://github.com/NovaCript"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
