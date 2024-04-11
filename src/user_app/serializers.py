1ebb6deb9c2d7d7b4eb852ede2f557fe26b138de
frutez <glebka.yatsenko@gmail.com>
2024-04-11 16:59:33 +0300

    update: hash password

--- src/user_app/serializers.py
+++ src/user_app/serializers.py
@@ -14,3 +14,17 @@ class UserSerializer(serializers.ModelSerializer):
                     'write_only': True,
                 }
         }
+
+    def create(self, validated_data):
+        user = User(
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.set_password(validated_data['password'])
+        instance.save()
+        return instance
